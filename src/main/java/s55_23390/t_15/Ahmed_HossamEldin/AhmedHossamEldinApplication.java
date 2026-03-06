50637a517186409f34d9f8afc39d404b98307c01
Ahmed HossamEldin <hossamahmedelsherbiny9@gmail.com>
2026-03-06 21:58:53 +0200

    Initial commit: project setup

---
+++ src/main/java/s55_23390/t_15/Ahmed_HossamEldin/AhmedHossamEldinApplication.java
@@ -0,0 +1,13 @@
+package s55_23390.t_15.Ahmed_HossamEldin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmedHossamEldinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhmedHossamEldinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s55_23390/t_15/Ahmed_HossamEldin/AhmedHossamEldinApplicationTests.java
@@ -0,0 +1,13 @@
+package s55_23390.t_15.Ahmed_HossamEldin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmedHossamEldinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
